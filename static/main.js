01b2cb4f05c09546c39386405499db0ed58ac75f
rvshcherb <shcherbinin_rv@mail.ru>
2019-08-25 08:46:59 +0300

    second commit

--- static/main.js
+++ static/main.js
@@ -1,3 +1,13 @@
+function showStocks() {
+  return ApiConnector.getStocks((err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
+}
+
 class Profile {
   constructor(user) {
     this.username = user.username;
@@ -6,15 +16,70 @@ class Profile {
   }
 
 
-  createUser() {
+  createUser(callback) {
     console.log(`Creating user ${this.username}`);
     return ApiConnector.createUser({
       username: this.username,
       name: this.name,
       password: this.password
-    }, console.log);
+    }, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`${this.name.firstName} is created`);
+        //console.log(data);
+        callback();
+      }
+    });
+  }
+
+  authorize(callback) {
+    console.log(`Authorizing user ${this.username}`);
+    return ApiConnector.performLogin({
+      username: this.username,
+      password: this.password,
+    }, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`${this.name.firstName} is authorized`);
+        callback();
+      }
+    });
+  }
+
+  addMoney(currency, amount, callback) {
+    console.log(`Adding ${amount} of ${currency} to ${this.username}`);
+    return ApiConnector.addMoney({
+      currency: currency,
+      amount: amount,
+    }, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(data);
+        console.log(`Added ${amount} of ${currency} to ${this.name.firstName}`);
+        callback();
+      }
+    });
+  }
+
+  convertMoney(fromCurrency, targetCurrency, targetAmont) {
+    return ApiConnector.convertMoney({
+      fromCurrency: fromCurrency,
+      targetCurrency: targetCurrency,
+      targetAmont: targetAmont
+    }, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(data);
+      }
+    });
   }
 }
+
+
 
 function main() {
   const Ivan = new Profile({
@@ -26,8 +91,17 @@ function main() {
     password: 'ivanspass',
   });
 
+  const Petr = new Profile({
+    username: 'petya',
+    name: {
+      firstName: 'Petr',
+      lastName: 'Petrov'
+    },
+    password: 'petrpass',
+  });
 
-  Ivan.createUser();
+ Ivan.createUser(() => Ivan.authorize(() => Ivan.addMoney('EUR', 100000, () => Ivan.convertMoney('EUR', 'NETCOIN', 1))));
 }
 
+showStocks();
 main();
